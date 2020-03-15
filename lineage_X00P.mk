d9e754ed92d3af08bfabdfb409bb2ce83bc1075f
MD Raza <raza231198@gmail.com>
2020-03-15 11:49:54 +0530

    X00P: initial commit

---
+++ lineage_X00P.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Inherit from X00P device
+$(call inherit-product, device/asus/X00P/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X00P
+PRODUCT_NAME := lineage_X00P
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X00PD
+PRODUCT_MANUFACTURER := asus
+TARGET_VENDOR := asus
+BOARD_VENDOR := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8937_64-user 9 PPR1.180610.009 16.00.1906.411-20190712 release-keys"
+
+# Set BUILD_FINGERPRINT variable
+BUILD_FINGERPRINT := asus/WW_X00PD/ASUS_X00P_4:9/PPR1.180610.009/16.00.1906.411:user/release-keys
